3b912f743fabf04efb641221376eef1abec16be4
Aleksey K <kropotin_aleksey@mail.ru>
2025-07-02 18:44:10 +0400

    fix

--- express/api/index.ts
+++ express/api/index.ts
@@ -1,9 +1,14 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/about", function (req, res) {
+  res.send("about");
 });
 
 export default app;
